a7d88af8aa5932a85875e6255b3380f3cc710650
Jaroslaw Nowacki <j.nowackii@o2.pl>
2020-11-26 09:54:43 +0100

    Todo App to test in postman without view layer

---
+++ src/main/java/pl/nowacki/todo/controller/InfoController.java
@@ -0,0 +1,30 @@
+package pl.nowacki.todo.controller;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.nowacki.todo.TaskConfigurationProperties;
+
+@RestController
+@RequestMapping("/info")
+class InfoController {
+    private DataSourceProperties dataSource;
+    private TaskConfigurationProperties myProp;
+
+    InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
+
+    @GetMapping("/url")
+    String url() {
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/prop")
+    boolean myProp() {
+        return myProp.getTemplate().isAllowMultipleTasks();
+    }
+}
+
---
+++ src/main/java/pl/nowacki/todo/logic/TaskGroupService.java
@@ -0,0 +1,47 @@
+package pl.nowacki.todo.logic;
+
+import pl.nowacki.todo.model.Project;
+import pl.nowacki.todo.model.TaskGroup;
+import pl.nowacki.todo.model.TaskGroupRepository;
+import pl.nowacki.todo.model.TaskRepository;
+import pl.nowacki.todo.projection.GroupReadModel;
+import pl.nowacki.todo.projection.GroupWriteModel;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TaskGroupService {
+    private TaskGroupRepository repository;
+    private TaskRepository taskRepository;
+
+    TaskGroupService(final TaskGroupRepository repository, final TaskRepository taskRepository) {
+        this.repository = repository;
+        this.taskRepository = taskRepository;
+    }
+
+    public GroupReadModel createGroup(final GroupWriteModel source) {
+        return createGroup(source, null);
+    }
+
+    GroupReadModel createGroup(final GroupWriteModel source, final Project project) {
+        TaskGroup result = repository.save(source.toGroup(project));
+        return new GroupReadModel(result);
+    }
+
+    public List<GroupReadModel> readAll() {
+        return repository.findAll().stream()
+                .map(GroupReadModel::new)
+                .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(int groupId) {
+        if (taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)) {
+            throw new IllegalStateException("Group has undone tasks. Done all the tasks first");
+        }
+        TaskGroup result = repository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("TaskGroup with given id not found"));
+        result.setDone(!result.isDone());
+        repository.save(result);
+    }
+}
+
